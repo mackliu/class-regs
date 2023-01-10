ed5d8c2bf6025f8682bdb69ed9ef2e85154006af
mackliu <macklun@gmail.com>
2023-01-10 16:03:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import { onMounted ,reactive } from 'vue';
+
+import  axios  from 'axios'
+
+const events=reactive([])
+
+
+
+onMounted(()=>{
+
+  axios.get('https://kktix.com/events.json')
+       .then(response=>{
+        return response.data;
+       })
+       .then(data=>{
+          data.entry.forEach(e=>{
+            events.push(e)
+          })
+       })
+
+})
+
+
+</script>
+
+<template>
+<ul>
+  <li v-for="e,i in events " :key="i">
+    {{ e.author.name }}
+  </li>
+</ul>
+</template>
+
+
